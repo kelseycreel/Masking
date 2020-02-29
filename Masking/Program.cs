496be377c16d2ce5b55c557b719377ca663455ee
Kelsey Creel <kelseycreel@gmail.com>
2020-02-29 12:17:16 -0600

    Add project files.

---
+++ Masking/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Masking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
